9889168a1473bf15266820f7fa6fb41fc90aed02
Ashton Pate <arpate@amarillocollege.com>
2025-01-19 15:49:10 -0800

    written line works

--- Wk1Ex4/Program.cs
+++ Wk1Ex4/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+Console.WriteLine(" Enter length of the rectangle: ");
+
+Console.ReadLine();
+
+
+Console.WriteLine(" Enter width of the rectangle: ");
+
+Console.ReadLine();
+
+Console.WriteLine(" Results: ");
+
+Console.WriteLine(" Area of the Rectangle: ");
+
+Console.WriteLine(" Perimeter of the Retangle: ");
