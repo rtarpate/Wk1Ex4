068234921cdab7878e5ce85e95f7bfe5198a3cef
Ashton Pate <arpate@amarillocollege.com>
2025-01-19 15:57:41 -0800

    commented the code

--- Wk1Ex4/Program.cs
+++ Wk1Ex4/Program.cs
@@ -1,21 +1,21 @@
 ﻿
-
+// gets the lenght of rectangle
 Console.WriteLine(" Enter length of the rectangle: ");
 
-
+// declares the lenght variable
 int lenght = int.Parse(Console.ReadLine());
 
-
+// gets the width of the rectangle
 Console.WriteLine(" Enter width of the rectangle: ");
 
-
+// declares the width variable
 int width = int.Parse(Console.ReadLine());
 
-
+// writes the results
 Console.WriteLine(" Results: ");
 
-
+// caulates the area of the rectangle
 Console.WriteLine(" Area of the Rectangle: " +  lenght * width );
 
-
+// cauclates the perimeter of the rectangle
 Console.WriteLine(" Perimeter of the Retangle: " + 2*(lenght + width) );
